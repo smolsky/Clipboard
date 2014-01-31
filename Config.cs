2256569a3b895fbf2148a6f62978f03eea264784
smolsky <smolskye@mail.ru>
2014-01-31 10:40:05 +0400

    Initial commit

---
+++ Config.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections.ObjectModel;
+
+namespace Clipboard
+{
+  
+  public class Config
+  {
+    public class Item
+    {
+      public string Name { get; set; }
+      public string Content { get; set; }
+      public override string ToString()
+      {
+        var format = "{0}: {1}";
+        if( String.IsNullOrEmpty(Name) || string.IsNullOrEmpty(Content) )
+          format = "{0}{1}";
+        var res = String.Format(format, Name, Content);
+        if (String.IsNullOrEmpty(res))
+          return "Empty";
+        return res;
+      }
+    };
+
+    public ObservableCollection<Item> Items { get; set; }
+
+    private Config() 
+    {
+      Items = new ObservableCollection<Item>();
+    }
+
+    private static String FilePath
+    {
+      get
+      {
+        return "Config.xml";
+      }
+    }
+    public void Save()
+    {
+      using (var fs = new FileStream(FilePath, FileMode.OpenOrCreate))
+      {
+        var xmlSerializer = new XmlSerializer(typeof(Config));
+
+        xmlSerializer.Serialize(fs, this);
+
+        fs.Flush();
+        fs.Close();
+      }
+    }
+
+    public static Config Load()
+    {
+      if (!File.Exists(FilePath))
+        return new Config();
+      try
+      {
+        using (var fs = new FileStream(FilePath, FileMode.Open))
+        {
+          var xmlSerializer = new XmlSerializer(typeof(Config));
+
+          var cfg = xmlSerializer.Deserialize(fs) as Config;
+
+          fs.Close();
+          return cfg;
+        }
+      }
+      catch (Exception)
+      {
+        return new Config();
+      }
+    }
+  }
+}
---
+++ CopyCliboardForm.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clipboard
+{
+  /// <summary>
+  /// Interaction logic for Window1.xaml
+  /// </summary>
+  public partial class CopyCliboardForm : Window
+  {
+    Config _cfg;//CopyCliboardForm.xaml
+    public CopyCliboardForm()
+    {
+      InitializeComponent();
+      _cfg = Config.Load();
+      DataContext = _cfg.Items;
+    }
+
+    private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+    {
+      var item = new Config.Item()
+      {
+        Name = _tbName.Text,
+        Content = _tbContent.Text
+      };
+      _cfg.Items.Add(item);
+    }
+
+    private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      var item = _lbItems.SelectedItem as Config.Item;
+      //System.Windows.Forms.Clipboard.SetText("");
+      if (item == null) return;
+      System.Windows.Forms.Clipboard.SetText(item.Content);
+    }
+
+    private void _this_Closed(object sender, EventArgs e)
+    {
+      _cfg.Save();
+    }
+  }
+}
